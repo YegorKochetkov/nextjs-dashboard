0217881ee4392e9d1371bd257d6b2f0c362e31b6
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-04-18 00:46:58 +0300

    chore: remove console log, reduce debounce time

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,8 +10,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter()
 
   const handleSearch = useDebouncedCallback((term: string) => {
-    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set("page", '1');
 
     if (term) {
       params.set('query', term);
@@ -20,7 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params.toString()}`)
-  }, 500);
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
