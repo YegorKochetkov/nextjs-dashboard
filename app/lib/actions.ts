92eb825d177775858f92d043b7648ced61da5357
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2024-04-22 00:23:12 +0300

    fix: check integer for amount

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -11,6 +11,7 @@ const FormSchema = z.object({
   }),
   amount: z.coerce
     .number()
+    .int()
     .gt(0, { message: 'Please enter an amount greater than $0.' }),
   status: z.enum(['pending', 'paid'], {
     invalid_type_error: 'Please select an invoice status.',
